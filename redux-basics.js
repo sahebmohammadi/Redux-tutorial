2cb66730bfc7c3baf8cfdffb25b9b4d05459cfa3
sahebmohammadi <saheb.put92@gmail.com>
2021-01-16 14:41:18 +0330

    two actions in one reducer

--- redux-basics.js
+++ redux-basics.js
@@ -1,16 +1,24 @@
 const redux = require("redux");
-const store = redux.createStore();
+// import redux from "redux";
+const createStore = redux.createStore;
 const BUY_CAKE = "BUT_CAKE";
-
+const BUY_ICECREAM = "BUY_ICECREAM";
 //? ACTION
 function buyCake() {
   return {
     type: BUY_CAKE,
   };
 }
+function buyIcecream() {
+  return {
+    type: BUY_ICECREAM,
+    value: 5,
+  };
+}
 //? REDUCER : (prevState,action)=> newState
 const initialState = {
   numberOfCakes: 10,
+  numberOfIcecreams: 20,
 };
 
 const reducer = (state = initialState, action) => {
@@ -20,9 +28,23 @@ const reducer = (state = initialState, action) => {
         ...state,
         numberOfCakes: state.numberOfCakes - 1,
       };
+    case "BUY_ICECREAM":
+      return {
+        ...state,
+        numberOfIcecreams: state.numberOfIcecreams - action.value,
+      };
     default:
       return state;
   }
 };
 
-//?
+//? STORE
+const store = createStore(reducer);
+
+//? DISPATCHING AN ACTION :
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+store.dispatch(buyCake());
+store.dispatch(buyIcecream());
+
+//? SUBSCRIPTION :
+unsubscribe();
